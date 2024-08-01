0a130b7bf536f4f3fc4545902268690666d5e1c3
Nongfsq <liyoh3366xm@gmail.com>
2024-07-31 22:29:07 -0700

    Add nvim configuration files

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+-- print("1. Start of init.lua")
+
+-- print("2. Before loading LazyVim")
+-- 加载 LazyVim 配置
+require("config.lazy")
+
+-- print("3. After loading LazyVim")
+
+-- print("4. End of init.lua")
