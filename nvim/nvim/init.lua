a9b2ed63877ba1f67eb4e27970c42f8a128e9322
Nongfsq <liyoh3366xm@gmail.com>
2024-07-31 22:41:05 -0700

    Auto-sync nvim configuration files

--- nvim/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- print("1. Start of init.lua")
-
--- print("2. Before loading LazyVim")
--- 加载 LazyVim 配置
-require("config.lazy")
-
--- print("3. After loading LazyVim")
-
--- print("4. End of init.lua")
